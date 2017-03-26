705d34a3b747bf73d50cfebc5e22fbb27c5761dd
David Stanley <davearthur.stanley@gmail.com>
2017-03-25 22:58:10 -0400

    iss #7 Updated xp_PlotData.m with specific functionality for rastergrams (unpacks multiple populations)

--- library_plots/xp_PlotData.m
+++ library_plots/xp_PlotData.m
@@ -13,6 +13,21 @@ function xp_PlotData (xp, op)
     xlims = op.xlims;
     ylims = op.ylims;
     
+    % If doing rastergrams or raster, re-add axis for populations
+    plot_type = [];
+    ind = find(strcmp(op.args,'plot_type'));
+    if ~isempty(ind); plot_type = op.args{ind+1}; end
+    if any(strcmp(plot_type,{'rastergram','raster'}))
+        if isfield(xp.meta,'matrix_dim_3')
+            if strcmp(xp.meta.matrix_dim_3.name,'populations')
+                %Nd = ndims(xp.data{1});
+                Nd = 3;
+                xp = xp.unpackDim(Nd);
+            end
+        end
+    end
+
+    
     % Add dummy axis for variables
     Na = length(xp.axis);
     xp.axis(Na+1).name = 'variables';
