a65d2bcf141a53cefe2580c072f0f836976e39ae
benp <benpolletta@gmail.com>
2017-04-27 19:46:05 -0400

    nDDictAxis & xp_tight_subplot_adaptive: removing findAxes method.

--- classes/nDDictAxis.m
+++ classes/nDDictAxis.m
@@ -7,52 +7,46 @@ classdef nDDictAxis
         values = []       % (optional) 1xN - can be numerical matrix or cell array of strings describing axis in question
         astruct = struct   % (optional) 1xN structure array of additional information
     end
-    
+
     methods
         function out = getaxisinfo(obj,show_class)
             if nargin < 2
                 show_class = 1;
             end
-            
+
             max_values_to_display = 10;
-            
+
             if isempty(obj.values); out = 'Empty axis'; return; end
-            
+
             % Add type
             values_class = obj.getclass_values;
-            
+
             if show_class
                 temp = [obj.name, ' (' values_class ')  -> '];
             else
                 temp = [obj.name, ' -> '];
             end
-            
+
             Nvals = length(obj.values);
             Nvals = min(Nvals,max_values_to_display);          % Limit to displaying 10 values
-            
+
             for i = 1:Nvals-1
                 temp = [temp,obj.getvaluestring(i),', '];
             end
-            
+
             if length(obj.values) > max_values_to_display
                 temp = [temp,obj.getvaluestring(Nvals),', ...'];
             else
                 temp = [temp,obj.getvaluestring(Nvals)];
             end
-            
+
             if nargout > 0
                 out = temp;
             else
                 fprintf([temp, '\n'])
             end
         end
-        
-        function out = findAxes(obj, axis_string_ref)
-            % Finds numbers of axis matching a given regular expression.
-            allnames = {obj.name};
-            out = regex_lookup(allnames, axis_string_ref);
-        end
-        
+
         function out = getvaluenoncell(obj,i)
             % Looks at entry obj.value(i) and returns its output as a
             % numeric, regardless of whether it is actually cell array.
@@ -63,7 +57,7 @@ classdef nDDictAxis
                 out = obj.values(i);
             end
         end
-        
+
         function out = getvaluestring(obj,i)
             % Looks at entry obj.value(i) and returns its output as a
             % string, regardless of what data type it actually is (string,
@@ -74,7 +68,7 @@ classdef nDDictAxis
                 out = num2str(out);
             end
         end
-        
+
         function out = getvaluescellstring(obj)
             % Looks at entry obj.value(i) and returns its output as a
             % cell array of strings
@@ -83,15 +77,15 @@ classdef nDDictAxis
                 out{i} = num2str(obj.getvaluenoncell(i));
             end
         end
-        
+
         function out = getclass_values(obj)
             out = obj.calcClasses(obj.values,'values');
         end
-        
+
         function out = getclass_name(obj)
             out = obj.calcClasses(obj.name,'name');
         end
-        
+
         function out = calcClasses(obj,input,field)
             switch field
                 case 'values'
@@ -114,24 +108,24 @@ classdef nDDictAxis
                     error('Unrecognized input foramt');
             end
         end
-        
-        %% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+
+        %% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
         % % % % % % % % % % % OVERLOADED OPERATORS % % % % % % % % % % %
         % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
         function varargout = subsref(varargin)
-            
+
 %             % Default settings for everything
 %             [varargout{1:nargout}] = builtin('subsref',varargin{:});
-            
+
             obj = varargin{1};
             S = varargin{2};
-            
+
             switch S(1).type
-                
+
                 case '()'
 %                     % Default
-%                     [varargout{1:nargout}] = builtin('subsref',varargin{:});                        
-                    
+%                     [varargout{1:nargout}] = builtin('subsref',varargin{:});
+
                     allnames = {obj.name};
                     if iscellstr(S(1).subs)
                         [selection_out, startIndex] = regex_lookup(allnames, S(1).subs{1});
@@ -139,9 +133,9 @@ classdef nDDictAxis
                         [varargout{1:nargout}] = builtin('subsref',obj,S,varargin{3:end});
                     else
                         % Default
-                        [varargout{1:nargout}] = builtin('subsref',varargin{:});                        
+                        [varargout{1:nargout}] = builtin('subsref',varargin{:});
                     end
-                    
+
                 case '{}'
                     % Default
                     [varargout{1:nargout}] = builtin('subsref',varargin{:});
@@ -151,7 +145,7 @@ classdef nDDictAxis
                 otherwise
                     error('Unknown indexing method. Should never reach this');
             end
-             
+
         end
 
     end
@@ -163,9 +157,9 @@ function [selection_out, startIndex] = regex_lookup(vals, selection)
     end
     if ~ischar(selection); error('Selection must be string when using regexp');
     end
-    
+
     startIndex = regexp(vals,selection);
     selection_out = logical(~cellfun(@isempty,startIndex));
     selection_out = find(selection_out);
-    
+
 end
