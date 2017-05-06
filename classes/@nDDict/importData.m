fbc1cced26528ca69f7bfcd27e89d8c908252980
David Stanley <davearthur.stanley@gmail.com>
2017-05-06 18:05:13 -0400

    iss #28 Moved importData to a separate file as well.

---
+++ classes/@nDDict/importData.m
@@ -0,0 +1,22 @@
+function obj = importData(obj,data,axis_vals,axis_names)
+            
+obj.data_pr = data;
+obj = obj.fixAxes;
+
+if nargin > 2
+    if ~isempty(axis_vals)
+        if ~iscell(axis_vals); error('axis_vals must be a cell array.'); end
+        for i = 1:length(axis_vals)
+            obj.axis_pr(i).values = axis_vals{i};
+        end
+        obj.checkDims;
+    end
+end
+
+if nargin > 3
+    if ~isempty(axis_names)
+        obj = obj.importAxisNames(axis_names);
+    end
+end
+
+end
--- classes/@nDDict/nDDict.m
+++ classes/@nDDict/nDDict.m
@@ -269,28 +269,8 @@ classdef nDDict
         obj = importLinearData(obj,X,varargin)            % Function for importing data in a linear format
         
         
-        function obj = importData(obj,data,axis_vals,axis_names)
-            
-            obj.data_pr = data;
-            obj = obj.fixAxes;
-            
-            if nargin > 2
-                if ~isempty(axis_vals)
-                    if ~iscell(axis_vals); error('axis_vals must be a cell array.'); end
-                    for i = 1:length(axis_vals)
-                        obj.axis_pr(i).values = axis_vals{i};
-                    end
-                    obj.checkDims;
-                end
-            end
-            
-            if nargin > 3
-                if ~isempty(axis_names)
-                    obj = obj.importAxisNames(axis_names);
-                end
-            end
-            
-        end
+        obj = importData(obj,data,axis_vals,axis_names)
+
         
         
         function out = exportAxisVals(obj)
