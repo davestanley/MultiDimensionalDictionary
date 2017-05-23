d3d2c2225acac6d8b5638560f008657f93e472af
Erik A. Roberts <erik-roberts@users.noreply.github.com>
2017-05-23 14:04:45 -0400

    Iss69 ref wrapper (#70)
    * iss #69 add ref wrapper class
    
    * update typo
    
    * overload methods method

---
+++ classes/MDDRef.m
@@ -0,0 +1,69 @@
+%% MDDRef - a copyable handle/reference class wrapper for MDD
+%
+% Purpose: unlike the base MDD class, this class permits...
+%   1) pass-by-reference (to avoid copying entire object when passing to functions as arg)
+%   2) event-driven callbacks in a subclass of MDDRef
+%
+% Usage: MDDRef objects have the same interface as MDD objects.
+%
+% Note: Due to the wrapper, tab completion show the available MDD methods. 
+%       However, those methods are still accessible when used.
+%
+% See also: MDD
+
+classdef MDDRef < matlab.mixin.Copyable
+    
+    properties (Access=private)
+        baseObj
+    end
+    
+    properties (Dependent)
+        data
+        axis
+    end
+    
+    methods
+        %         % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+        %         % % % % % % % % % % % Getter and Setters % % % % % % % % % % % %
+        %         % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+        function obj = MDDRef(varargin)
+            obj.baseObj = MDD(varargin{:});
+        end
+        
+        function value = get.data(obj)
+            value = obj.baseObj.data;
+        end
+        
+        function varargout = get.axis(obj)
+            [varargout{1:nargout}] = obj.baseObj.axis;
+        end
+        
+        %% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+        % % % % % % % % % % % OVERLOADED OPERATORS % % % % % % % % % % %
+        % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+        function varargout = subsref(obj, S)
+            if strcmp(S(1).type, '.') && (strcmp(S(1).subs, 'copy') || strcmp(S(1).subs, 'methods'))
+                [varargout{1:nargout}] = builtin('subsref', obj, S);
+            else
+                [varargout{1:nargout}] = builtin('subsref', obj.baseObj, S);
+                if isa(varargout{1}, 'MDD') && ~strcmp(S(1).type, '()')
+                    obj.baseObj = varargout{1};
+                end
+            end
+        end
+
+        function obj = subsasgn(obj, S, value)
+            obj.baseObj = builtin('subsasgn', obj.baseObj, S, value);
+        end
+        
+        function mObj = methods(obj)
+            mObj = builtin('methods', obj);
+            mBaseObj = methods(obj.baseObj);
+            mBaseObj(strcmp(mBaseObj, class(obj.baseObj))) = []; % remove class name
+            
+            mObj = unique([mObj;mBaseObj]);
+        end
+        
+    end
+    
+end
