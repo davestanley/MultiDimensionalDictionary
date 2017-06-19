bbd9a0730c9c2997cbb8dd91ac00f8e1b4a287a6
David Stanley <davearthur.stanley@gmail.com>
2017-06-19 05:43:09 -0400

    Added temporary fix in xp_PlotData to accommodate bug in dsPlot.

--- library_plots/xp_PlotData.m
+++ library_plots/xp_PlotData.m
@@ -38,6 +38,24 @@ function hxp = xp_PlotData (xp, op)
     
     % Feed into original PlotData command, making sure it doesn't generate
     % new figures (rather, should produce it in the current subplot)
+    
+    % Hack to get working with dsPlot bug being unable to accept strings
+    % right now. This issue results around line 200 (seems to have to
+    % dowith the new code for detecting co-varied params in dsPlot).
+    if isfield(data(1),'varied')
+        varied = data(1).varied;
+        for i = 1:length(data);
+            for j = 1:length(varied)
+                if ischar(data(i).(varied{j}))
+                    temp = data(i).(varied{j});
+                    ind = strfind(temp,'_');
+                    temp = str2num(temp(1:ind-1));
+                    if isempty(temp); temp = 0; end
+                    data(i).(varied{j}) = temp;
+                end
+            end
+        end
+    end
     hxp.hcurr = dsPlot(data,op.args{:},'lock_gca',true);
     
     if ~isempty(xlims); xlim(xlims); end
